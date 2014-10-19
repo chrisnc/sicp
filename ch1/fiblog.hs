db84f94305e871697936244b3d4592ecd6c33143
Chris Copeland <chrisnc@cs.stanford.edu>
2014-10-19 01:23:03 -0700

    minor changes to fiblog.hs

--- ch1/fiblog.hs
+++ ch1/fiblog.hs
@@ -5,6 +5,12 @@
  a' <- (q + p)a + qb
  b' <- qa + pb
 
+ a'' <- (q + p)a' + qb'
+ b'' <- qa' + pb'
+
+ a'' <- (q + p)((q + p)a + qb) + q(qa + pb)
+ b'' <- q((q + p)a + qb) + p(qa + pb)
+
  a'' <- (2q^2 + 2qp + p^2) a + (q^2 + 2qp)b
  b'' <- (q^2 + 2qp)a + (q^2 + p^2)b
 
@@ -14,9 +20,7 @@
 --}
 
 fib = iter 1 0 0 1 where
-  iter a b p q count
-    | count == 0 = b
-    | even count =
-        iter a b (q^2 + p^2) (q^2 + 2*q*p) (count `div` 2)
-    | otherwise  =
-        iter ((q + p)*a + q*b) (q*a + p*b) p q (count - 1)
+  iter a b p q n
+    | n == 0    = b
+    | even n    = iter a b (q^2 + p^2) (q^2 + 2*q*p) (n `div` 2)
+    | otherwise = iter ((q + p)*a + q*b) (q*a + p*b) p q (n - 1)
