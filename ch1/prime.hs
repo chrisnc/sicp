2e87dd0e44e081458f35207211a8edbcfde9a8f7
Chris Copeland <chrisnc@cs.stanford.edu>
2014-10-29 02:26:26 -0700

    implemented fermat prime test in Haskell using System.Random

--- ch1/prime.hs
+++ ch1/prime.hs
@@ -1,5 +1,7 @@
 -- SICP Section 1.2.6 Example, Testing for Primality
 
+import System.Random
+
 square x = x * x
 
 smallestDivisor n = findDivisor n 2
@@ -12,3 +14,15 @@ findDivisor n testDivisor
 divides a b = rem b a == 0
 
 prime n = n == smallestDivisor n
+
+expmod base exp m
+  | exp == 0  = 1
+  | even exp  = rem (square (expmod base (exp `div` 2) m)) m
+  | otherwise = rem (base * (expmod base (exp - 1) m)) m
+
+fermatTest n a = expmod a n n == a
+
+fastPrime n times rgen =
+  and $ map (fermatTest n) $ take times $ randomRs (1, n - 1) rgen
+
+fastPrimeIO n times = fmap (fastPrime n times) newStdGen
