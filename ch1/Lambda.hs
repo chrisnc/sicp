c6679206bac3e7290730d1031e8b755db9078a7d
Chris Copeland <chrisnc@cs.stanford.edu>
2014-11-09 21:12:48 -0800

    finished chapter 1 in haskell and scheme

--- ch1/Lambda.hs
+++ ch1/Lambda.hs
@@ -13,6 +13,8 @@ fWithLet x y =
       b = 1 - y
   in x * a * a + y * b + a * b
 
+square x = x * x
+
 -- Exercise 1.34, page 88
 f :: Num a => (a -> t) -> t
 f g = g 2
@@ -29,8 +31,10 @@ f g = g 2
 
 closeEnough x y = abs (x - y) < 0.001
 
+average x y = (x + y) / 2
+
 search f negp posp =
-  let mid = (negp + posp) / 2
+  let mid = average negp posp
   in if closeEnough negp posp
     then mid
     else let testVal = f mid
@@ -38,3 +42,147 @@ search f negp posp =
                | testVal < 0 = search f mid posp
                | otherwise = mid
       in r
+
+halfIntervalMethod f a b = r where
+  aval = f a
+  bval = f b
+  r | aval < 0 && bval > 0 = search f a b
+    | bval < 0 && aval > 0 = search f b a
+    | otherwise =
+        error $ "Values are not of opposite sign " ++ show a ++ " " ++ show b
+
+tolerance = 0.00001
+
+fixedPoint f firstGuess = try firstGuess where
+  closeEnough v1 v2 = abs (v1 - v2) < tolerance
+  try guess = let next = f guess in
+    if closeEnough guess next then next else try next
+
+sqrtFixed x = fixedPoint (\y -> average y (x / y)) 1.0
+
+-- Exercise 1.35, page 94
+-- x -> 1 + 1/x
+-- x = 1 + 1/x
+-- x^2 = x + 1
+-- x^2 - x - 1 = 0
+-- x = (1 + sqrt(1 + 4)) / 2
+-- x = (1 + sqrt(5)) / 2 = phi
+
+phi = fixedPoint (\x -> 1 + (1 / x)) 1.0
+
+-- Exercise 1.36, page 94
+fixedPointTrace f firstGuess = try firstGuess where
+  closeEnough v1 v2 = abs (v1 - v2) < tolerance
+  try guess = do
+    putStrLn $ show guess
+    let next = f guess
+    if closeEnough guess next
+      then return next
+      else try next
+
+runxxsol = do
+  fixedPointTrace (\x -> log 1000 / log x) 2.0
+  fixedPointTrace (\x -> (x + log 1000 / log x) / 2.0) 2.0
+
+-- Exercise 1.37 (a), page 94
+contFrac n d k = cfrack k 0 where
+  cfrack i result =
+    if i == 0
+      then result
+      else cfrack (i - 1) $ n i / (d i + result)
+
+phiContFrac = contFrac (const 1.0) (const 1.0) 100
+
+-- Exercise 1.37 (b), page 95
+-- recursive process version
+contFracRec n d k = cfrack 1 where
+  cfrack i =
+    if i == k
+      then n i / d i
+      else n i / (d i + cfrack (i + 1))
+
+-- Exercise 1.38, page 96
+eulerFrac k = contFrac (const 1.0) denom k where
+  denom i =
+    if rem (i - 2) 3 == 0
+      then (fromIntegral i + 1) * 2 / 3
+      else 1
+
+-- Exercise 1.39, page 96
+tanCf x k =
+  contFrac
+    (\i -> if i == 1 then x else negate (x * x))
+    (\i -> fromIntegral i * 2 - 1)
+    k
+
+-- Section 1.3.4, Procedures as Returned Values, page 97
+averageDamp f = \x -> average x (f x)
+
+sqrtFixedAvg x = fixedPoint (averageDamp (x /)) 1.0
+
+cubeRoot x = fixedPoint (averageDamp ((x /) . square)) 1.0
+
+dx = 0.00001
+
+deriv g x = (g (x + dx) - g x) / dx
+
+newtonTransform g x = x - g x / (deriv g $ x)
+
+newtonsMethod g = fixedPoint (newtonTransform g)
+
+sqrtNewton x = newtonsMethod ((subtract x) . square) 1.0
+
+fixedPointOfTransform g transform =
+  fixedPoint (transform g)
+
+sqrtFpotAvgDamp x = fixedPointOfTransform (x /) averageDamp 1.0
+
+sqrtFpotNewton x = fixedPointOfTransform ((subtract x) . square) newtonTransform 1.0
+
+-- Exercise 1.40, page 103
+-- because of partial application, this is entirely equivalent to using lambda
+cubic a b c x = x*x*x + a*x*x + b*x + c
+
+-- Exercise 1.41, page 103
+double f x = f (f x)
+
+-- Exercise 1.42, page 103
+compose f g x = f (g x)
+
+-- Exercise 1.43, page 104
+repeated f n = iter f n where
+  iter fr n =
+    if n == 1
+      then fr
+      else iter (compose f fr) (n - 1)
+
+-- Exercise 1.44, page 104
+smooth f x = (f (x - dx) + f x + f (x + dx)) / 3.0
+
+smoothN f n = (repeated smooth n) f
+
+-- Exercise 1.45, page 105
+nthRoot n x =
+  fixedPointOfTransform
+    (\y -> x / (y ** (n - 1)))
+    (repeated averageDamp 3)
+    x
+
+-- Exercise 1.46, page 105
+iterativeImprove goodEnough improve = iter where
+  iter x =
+    if goodEnough x
+      then x
+      else iter (improve x)
+
+sqrtImp x =
+  iterativeImprove
+    (\guess -> abs (square guess - x) < 0.001)
+    (\guess -> average guess (x / guess))
+    1.0
+
+fixedPointImp f =
+  iterativeImprove (\guess -> abs (guess - f guess) < tolerance) f
+
+newtonsMethodImp g guess =
+  fixedPointImp (newtonTransform g) guess
--- ch1/lambda.scm
+++ ch1/lambda.scm
@@ -98,7 +98,149 @@
         (try next))))
   (try first-guess))
 
-(define xx-sol
-  (fixed-point-trace (lambda (x) (/ (log 1000) (log x))) 2.0))
-(define xx-sol-avg
-  (fixed-point-trace (lambda (x) (/ (+ x (/ (log 1000) (log x))) 2.0)) 2.0))
+;(define xx-sol
+;  (fixed-point-trace (lambda (x) (/ (log 1000) (log x))) 2.0))
+;(define xx-sol-avg
+;  (fixed-point-trace (lambda (x) (/ (+ x (/ (log 1000) (log x))) 2.0)) 2.0))
+
+; Exercise 1.37 (a), page 94
+(define (cont-frac n d k)
+  (define (cfrack i result)
+    (if (= i 0)
+      result
+      (cfrack (- i 1) (/ (n i) (+ (d i) result)))))
+  (cfrack k 0))
+
+(define phi-cfrac (cont-frac (lambda (i) 1.0) (lambda (i) 1.0) 100))
+
+; Exercise 1.37 (b), page 95
+; recursive process version
+(define (cont-frac-rec n d k)
+  (define (cfrack i)
+    (if (= i k)
+      (/ (n i) (d i))
+      (/ (n i) (+ (d i) (cfrack (+ i 1))))))
+  (cfrack 1))
+
+; Exercise 1.38, page 96
+(define (euler-frac k)
+  (cont-frac
+    (lambda (i) 1.0)
+    (lambda (i)
+      (cond ((= 0 (remainder (- i 2) 3)) (/ (* 2 (+ i 1)) 3))
+            (else 1)))
+    k))
+
+; Exercise 1.39, page 96
+(define (tan-cf x k)
+  (cont-frac
+    (lambda (i)
+      (if (= i 1)
+        x
+        (- (* x x))))
+    (lambda (i) (- (* i 2) 1))
+    k))
+
+; Section 1.3.4, Procedures as Returned Values, page 97
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
+
+(define (sqrt-fixed-avg x)
+  (fixed-point (average-damp (lambda (y) (/ x y)))
+               1.0))
+
+(define (cube-root x)
+  (fixed-point (average-damp (lambda (y) (/ x (square y))))
+               1.0))
+
+(define dx 0.00001)
+
+(define (deriv g)
+  (lambda (x) (/ (- (g (+ x dx)) (g x)) dx)))
+
+(define (newton-transform g)
+  (lambda (x) (- x (/ (g x) ((deriv g) x)))))
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) guess))
+
+(define (sqrt-newton x)
+  (newtons-method
+    (lambda (y) (- (square y) x))
+    1.0))
+
+(define (fixed-point-of-transform g transform guess)
+  (fixed-point (transform g) guess))
+
+(define (sqrt-fpot-avg-damp x)
+  (fixed-point-of-transform
+    (lambda (y) (/ x y)) average-damp 1.0))
+
+(define (sqrt-fpot-newton x)
+  (fixed-point-of-transform
+    (lambda (y) (- (square y) x)) newton-transform 1.0))
+
+; Exercise 1.40, page 103
+(define (cubic a b c)
+  (lambda (x)
+    (+ (* x x x)
+       (* a x x)
+       (* b x)
+       c)))
+
+; Exercise 1.41, page 103
+(define (double f)
+  (lambda (x) (f (f x))))
+
+; Exercise 1.42, page 103
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+; Exercise 1.43, page 104
+(define (repeated f n)
+  (define (iter fr n)
+    (if (= n 1)
+      fr
+      (iter (compose f fr) (- n 1))))
+  (iter f n))
+
+; Exercise 1.44, page 104
+(define (smooth f)
+  (lambda (x)
+    (/ (+ (f (- x dx))
+          (f x)
+          (f (+ x dx)))
+       3.0)))
+
+(define (smooth-n f n)
+  ((repeated smooth n) f))
+
+; Exercise 1.45, page 105
+(define (nth-root n x)
+  (fixed-point-of-transform
+    (lambda (y) (/ x (expt y (- n 1))))
+    (repeated average-damp n)
+    x))
+
+; Exercise 1.46, page 105
+(define (iterative-improve good-enough improve)
+  (define (iter x)
+    (if (good-enough x)
+      x
+      (iter (improve x))))
+  iter)
+
+(define (sqrt-imp x)
+  ((iterative-improve
+    (lambda (guess) (< (abs (- (square guess) x)) 0.001))
+    (lambda (guess) (average guess (/ x guess))))
+  1.0))
+
+(define (fixed-point-imp f first-guess)
+  ((iterative-improve
+     (lambda (guess) (< (abs (- guess (f guess))) tolerance))
+     f)
+   first-guess))
+
+(define (newtons-method-imp g guess)
+  (fixed-point-imp (newton-transform g) guess))
