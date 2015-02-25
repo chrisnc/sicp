0f42e1e8ba3446ab65d2d346abb9612a06defd7d
Chris Copeland <chrisnc@cs.stanford.edu>
2015-02-25 00:44:21 -0800

    minor changes to Complex.hs and added exports

--- hs/Complex.hs
+++ hs/Complex.hs
@@ -1,7 +1,13 @@
 -- Section 2.4.1, Representations for Complex Numbers, page 232
 
 module Complex
-  (
+  ( Complex
+  , mkFromRealImag
+  , mkFromMagAng
+  , real
+  , imag
+  , mag
+  , ang
   ) where
 
 import GHC.Real (Ratio((:%)))
@@ -10,16 +16,16 @@ import GHC.Real (Ratio((:%)))
 -- this section.
 data Complex a = CRect a a | CPolar a a
 
-makeFromRealImag :: a -> a -> Complex a
-makeFromRealImag = CRect
+mkFromRealImag :: a -> a -> Complex a
+mkFromRealImag = CRect
 
-makeFromMagAng :: a -> a -> Complex a
-makeFromMagAng = CPolar
+mkFromMagAng :: a -> a -> Complex a
+mkFromMagAng = CPolar
 
 -- Pretty-printing for each representation of complex number.
 instance Show a => Show (Complex a) where
   show z = case z of
-    CRect x y -> show x ++ " + " ++ show y ++ "i"
+    CRect  x y -> show x ++ " + " ++ show y ++ "i"
     CPolar r a -> show r ++ "e^(" ++ show a ++ "i)"
 
 -- These functions implement the dispatching to different implementations of
@@ -43,19 +49,19 @@ mag z = case z of
 
 ang :: Floating a => Complex a -> a
 ang z = case z of
-  CRect  x y -> atan (y / x)
+  CRect  x y -> atan (y / x) -- TODO: this is incorrect, as atan only gives results between -pi/2 and pi/2
   CPolar _ a -> a
 
 -- Using the implementations of real, imag, mag, and ang, we can implement
 -- the Num and Fractional interfaces for our complex number type.
 instance Floating a => Num (Complex a) where
-  u + v = makeFromRealImag (real u + real v) (imag u + imag v)
-  u * v = makeFromMagAng   (mag u * mag v)   (ang u + ang v)
-  u - v = makeFromRealImag (real u - real v) (imag u - imag v)
-  abs z = makeFromRealImag (mag z) 0
+  u + v = mkFromRealImag (real u + real v) (imag u + imag v)
+  u * v = mkFromMagAng   (mag u * mag v)   (ang u + ang v)
+  u - v = mkFromRealImag (real u - real v) (imag u - imag v)
+  abs z = mkFromRealImag (mag z) 0
   signum z = z / abs z
-  fromInteger n = makeFromRealImag (fromInteger n) 0
+  fromInteger n = mkFromRealImag (fromInteger n) 0
 
 instance Floating a => Fractional (Complex a) where
-  u / v = makeFromMagAng (mag u / mag v) (ang u - ang v)
-  fromRational (n :% d) = makeFromRealImag (fromInteger n / fromInteger d) 0
+  u / v = mkFromMagAng (mag u / mag v) (ang u - ang v)
+  fromRational (n :% d) = mkFromRealImag (fromInteger n / fromInteger d) 0
