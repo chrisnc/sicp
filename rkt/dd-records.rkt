888e2e54508957d69d6330e5acd2655808453641
Chris Copeland <chrisnc@cs.stanford.edu>
2015-06-07 14:43:31 -0700

    use dd-common.rkt in dd-records to make it actually work

--- rkt/dd-records.rkt
+++ rkt/dd-records.rkt
@@ -1,7 +1,20 @@
 ; Exercise 2.74, page 250
 
-(define type-tag car)
-(define contents cdr)
+(require racket/include)
+(include "dd-common.rkt")
+
+(define (install-computer-division)
+  (define (get-record employee-name file)
+    (hash-ref file employee-name #f))
+  (define get-salary car)
+  (define (set-record file employee-name salary address)
+    (hash-set! file employee-name (cons salary address)))
+  (define create-file make-hash)
+  (put 'get-salary 'computer-division get-salary)
+  (put 'get-record 'computer-division get-record)
+  (put 'set-record 'computer-division set-record)
+  (put 'create-file 'computer-division create-file)
+  'done)
 
 ; part a
 (define (get-record employee-name generic-file)
@@ -15,8 +28,9 @@
 
 ; part b
 (define (get-salary employee-name generic-file)
-  (let ((record ((get 'get-record (type-tag generic-file)) employee-name)))
-    ((get 'get-salary (type-tag record)) (contents record))))
+  (let ((tag (type-tag generic-file)))
+    (let ((record ((get 'get-record tag) employee-name (contents generic-file))))
+      ((get 'get-salary tag) record))))
 
 ; part c
 (define (find-employee-record employee-name files)
@@ -24,6 +38,24 @@
     (let ((record (get-record employee-name (car files))))
       (if (null? record) (find-employee-record employee-name (cdr files))
         record))))
+
+; put it all together
+(define (set-record generic-file employee-name salary address)
+  (let ((tag (type-tag generic-file)))
+    ((get 'set-record tag) (contents generic-file) employee-name salary address)))
+
+(define (create-file tag)
+  (cons tag ((get 'create-file tag))))
+
+(install-computer-division)
+(define cfile (create-file 'computer-division))
+(set-record cfile "Bill" 123 "foo")
+(display (get-record "Bill" cfile))
+(newline)
+(display (get-salary "Bill" cfile))
+(newline)
+(display (find-employee-record "Bill" (list cfile)))
+(newline)
 
 ; part d
 ; The changes that must be made are to give the new division(s) a unique type
